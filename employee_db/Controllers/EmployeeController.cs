a35a7e7d6515ac706bf28558c8d7290a52174ad0
NBMErni <nikko.mallari@askbetter.erni>
2024-07-16 11:30:21 +0800

    clean-up

--- employee_db/Controllers/EmployeeController.cs
+++ employee_db/Controllers/EmployeeController.cs
@@ -15,25 +15,29 @@ namespace employee_db.Controllers
         {
             this.context = context;
         }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpGet("employees")]
+        // Get all employees from DB and convert it into Json
+        [HttpGet("employees")] // 
         public IActionResult GetEmployees()
         {
-            var employees = context.Employee.ToList();
-
-            var employeesJson = System.Text.Json.JsonSerializer.Serialize(employees);
-            System.Diagnostics.Debug.WriteLine("Employee DB TO >>>>> " + employeesJson);
-
-            //return Content(employeesJson, "application/json");
-            return Json(employees);
+            try
+            {
+                var employees = context.Employee.ToList();
+                return Json(employees);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal server error");
+            }
         }
 
-
-
+        // View for create page
         public IActionResult Create()
         {
             return View();
@@ -42,107 +46,130 @@ namespace employee_db.Controllers
         [HttpPost]
         public IActionResult Create(EmployeeDto EmployeeDto)
         {
-            // validation 
-            if (!ModelState.IsValid)
+            try
             {
-                return View(EmployeeDto);
+                // Validation 
+                if (!ModelState.IsValid)
+                {
+                    return View(EmployeeDto);
+                }
+
+                // Creating data obj to save in DB
+                EmployeeInfo emp_data = new EmployeeInfo()
+                {
+                    first_name = EmployeeDto.first_name,
+                    last_name = EmployeeDto.last_name,
+                    gender = EmployeeDto.gender,
+                    birth_date = EmployeeDto.birth_date,
+                    email = EmployeeDto.email,
+                    position = EmployeeDto.position,
+                };
+
+                // Passing the emp_data to DB and saving it
+                context.Employee.Add(emp_data);
+                context.SaveChanges();
+
+                return RedirectToAction("Index", "Employee");
             }
-
-            // Creating data obj to save in DB
-            EmployeeInfo emp_data = new EmployeeInfo()
+            catch (Exception ex)
             {
-                first_name = EmployeeDto.first_name,
-                last_name = EmployeeDto.last_name,
-                gender = EmployeeDto.gender,
-                birth_date = EmployeeDto.birth_date,
-                email = EmployeeDto.email,
-                position = EmployeeDto.position,
-             
-            };
-
-            // passing the emp_data to DB and saving it
-            context.Employee.Add(emp_data);
-            context.SaveChanges();
-
-            return RedirectToAction("Index", "Employee");
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal server error");
+            }
         }
 
         public IActionResult Edit(int id)
         {
-            var employee = context.Employee.Find(id);
-
-            if (employee == null)
+            try
             {
-                return RedirectToAction("Index", "Employee");
+                var employee = context.Employee.Find(id);
+
+                if (employee == null)
+                {
+                    return RedirectToAction("Index", "Employee");
+                }
+
+                var employeeDto = new EmployeeDto()
+                {
+                    first_name = employee.first_name,
+                    last_name = employee.last_name,
+                    gender = employee.gender,
+                    birth_date = employee.birth_date,
+                    email = employee.email,
+                    position = employee.position
+                };
+
+                ViewData["employee_id"] = employee.Id;
+
+                return View(employeeDto);
             }
-
-            var employeeDto = new EmployeeDto()
+            catch (Exception ex)
             {
-                first_name = employee.first_name,
-                last_name = employee.last_name,
-                gender = employee.gender,
-                birth_date = employee.birth_date,
-                email = employee.email,
-                position = employee.position
-
-            };
-
-            ViewData["employee_id"] = employee.Id;
-
-
-            return View(employeeDto);
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal server error");
+            }
         }
-
 
         [HttpPost]
         public IActionResult Edit(int id, EmployeeDto EmployeeDto)
         {
-            var employee = context.Employee.Find(id);
-
-            if (employee == null)
+            try
             {
+                var employee = context.Employee.Find(id);
+
+                if (employee == null)
+                {
+                    return RedirectToAction("Index", "Employee");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    ViewData["employee_id"] = employee.Id;
+                    return View(EmployeeDto);
+                }
+
+                // Update product details
+                employee.first_name = EmployeeDto.first_name;
+                employee.last_name = EmployeeDto.last_name;
+                employee.gender = EmployeeDto.gender;
+                employee.birth_date = EmployeeDto.birth_date;
+                employee.email = EmployeeDto.email;
+                employee.position = EmployeeDto.position;
+
+                context.SaveChanges();
+
                 return RedirectToAction("Index", "Employee");
             }
-
-            if (!ModelState.IsValid)
+            catch (Exception ex)
             {
-                ViewData["employee_id"] = employee.Id;
-
-
-                return View(EmployeeDto);
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal server error");
             }
-
-            // Update product details
-            employee.first_name = EmployeeDto.first_name;
-            employee.last_name = EmployeeDto.last_name;
-            employee.gender = EmployeeDto.gender;
-            employee.birth_date = EmployeeDto.birth_date;
-            employee.email = EmployeeDto.email;
-            employee.position = EmployeeDto.position;
-
-            context.SaveChanges();
-
-            return RedirectToAction("Index", "Employee");
-
         }
 
         [HttpDelete("delete-employee/{id:int}")]
         public IActionResult Delete(int id)
         {
-            var employee = context.Employee.Find(id);
-            if (employee == null)
+            try
             {
+                var employee = context.Employee.Find(id);
+                if (employee == null)
+                {
+                    return RedirectToAction("Index", "Employee");
+                }
+
+                context.Employee.Remove(employee);
+                context.SaveChanges(true);
+
                 return RedirectToAction("Index", "Employee");
             }
-
-
-
-            context.Employee.Remove(employee);
-            context.SaveChanges(true);
-
-            return RedirectToAction("Index", "Employee");
-
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal server error");
+            }
         }
+
 
     }
 }
